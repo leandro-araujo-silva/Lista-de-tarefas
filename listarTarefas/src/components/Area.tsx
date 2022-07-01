c1b8546d4936ff73288aea068b01accb94672cd7
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2022-07-01 18:01:21 -0300

    Adicionando

---
+++ listarTarefas/src/components/Area.tsx
@@ -0,0 +1,10 @@
+import styles from './Area.module.css'
+import { Header } from './Header'
+
+export function Area() {
+  return (
+    <div className={styles.area}>
+      <Header></Header>
+    </div>
+  )
+}
